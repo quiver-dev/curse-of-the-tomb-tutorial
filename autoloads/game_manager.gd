016ae373d32b11b2c3f09c1b0feb3be42be7635a
Joe Bustamante <josephmbustamante@gmail.com>
2023-07-11 22:08:16 -0400

    Video 57 - add a save system to the game, and load the save when starting the game up

--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -5,6 +5,11 @@ signal on_coins_changed(new_coin_amount: int)
 
 enum Upgrades { EXTRA_HEALTH_1, EXTRA_HEALTH_2, DOUBLE_JUMP, DASH, TELEKINESIS }
 
+var save_name := "save_1"
+var total_time_played := 0.0
+var is_playing := false
+var current_world := "res://world/spawn_room.tscn"
+
 var has_extra_health_1 := false
 var has_extra_health_2 := false
 var has_double_jump := false
@@ -19,6 +24,56 @@ var coins := 0:
 	set(value):
 		coins = value
 		on_coins_changed.emit(coins)
+
+
+func _enter_tree() -> void:
+	var save_data = SaveManager.load_game(save_name)
+	load_game(save_data)
+
+
+func _exit_tree() -> void:
+	SaveManager.save_game(save_name, create_save_data())
+
+
+func _process(delta: float) -> void:
+	if is_playing:
+		total_time_played += delta
+
+
+func load_game(save_data: Dictionary):
+	if save_data.is_empty():
+		return
+
+	max_health = save_data["max_health"]
+	health = max_health
+
+	coins = save_data["coins"]
+	has_extra_health_1 = save_data["has_extra_health_1"]
+	has_extra_health_2 = save_data["has_extra_health_2"]
+	has_double_jump = save_data["has_double_jump"]
+	has_dash = save_data["has_dash"]
+	has_telekinesis = save_data["has_telekinesis"]
+	save_name = save_data["save_name"]
+	total_time_played = save_data["total_time_played"]
+	current_world = save_data["current_world"]
+
+	is_playing = true
+	get_tree().change_scene_to_file(current_world)
+
+
+func create_save_data() -> Dictionary:
+	return {
+		"max_health": max_health,
+		"coins": coins,
+		"has_extra_health_1": has_extra_health_1,
+		"has_extra_health_2": has_extra_health_2,
+		"has_double_jump": has_double_jump,
+		"has_dash": has_dash,
+		"has_telekinesis": has_telekinesis,
+		"save_name": save_name,
+		"total_time_played": total_time_played,
+		"current_world": current_world,
+	}
 
 
 func has_upgrade(upgrade: Upgrades) -> bool:
@@ -72,6 +127,7 @@ func buy_upgrade(upgrade: Upgrades, cost: int, player: Player) -> bool:
 
 
 func change_scene(new_scene: PackedScene):
+	current_world = new_scene.resource_path
 	get_tree().change_scene_to_packed(new_scene)
 
 
---
+++ autoloads/save_manager.gd
@@ -0,0 +1,36 @@
+extends Node
+
+
+const SAVE_DIR = "user://saves/"
+const SAVE_EXTENSION = ".save"
+
+
+func _ready() -> void:
+	if not DirAccess.dir_exists_absolute(SAVE_DIR):
+		DirAccess.make_dir_absolute(SAVE_DIR)
+
+
+func save_game(save_name: String, save_data: Dictionary):
+	var save_path = get_save_path(save_name)
+	var save_file = FileAccess.open(save_path, FileAccess.WRITE)
+	if save_file != null:
+		save_file.store_var(save_data)
+		print("Game saved!")
+	else:
+		print("ERROR: could not save %s, error code %d" % [save_name, FileAccess.get_open_error()])
+
+
+func load_game(save_name: String) -> Dictionary:
+	var save_path = get_save_path(save_name)
+	var save_file = FileAccess.open(save_path, FileAccess.READ)
+	if save_file != null:
+		var save_data = save_file.get_var()
+		print("Game loaded!")
+		return save_data
+	else:
+		print("ERROR: could not load %s, error code %d" % [save_name, FileAccess.get_open_error()])
+		return {}
+
+
+func get_save_path(save_name: String) -> String:
+	return SAVE_DIR + save_name + SAVE_EXTENSION
